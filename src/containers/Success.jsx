2d18d1e4f54ef1c2c1574625f4acb87a7a3e98a3
Gerardo Zuno <gerardozunounitec@gmail.com>
2021-06-24 15:58:05 -0500

    Add Google Maps

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import Map from '../components/Map';
 import '../styles/components/Success.css';
 
-
-
 const Success = () => {
+  const { state } = useContext(AppContext);
+  const { buyer } = state;
+
   return (
-    <div className="Sucess">
-      <div className="Success content">
-        <h2>Gracias por tu compra</h2>
-        <span>Tu pedido llegara en 3 dias a tu direccion:</span>
+    <div className="Success">
+      <div className="Success-content">
+        <div className="Success-content-info">
+          <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
+          <span>Tu pedido llegara en 3 dias a tu direccion:</span>
+        </div>       
         <div className="Success-map">
-          Google
+          <Map />
         </div>
       </div>
-
     </div>
   );
 }
